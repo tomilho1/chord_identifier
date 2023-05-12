cb367aba2a824586ad5d96a601a1d7733466867b
Tom Francisco da Rosa <123115729+tomilho1@users.noreply.github.com>
2023-05-12 20:49:39 -0300

    Inversões e condicionais nos logs

--- chordIdentifier.js
+++ chordIdentifier.js
@@ -23,12 +23,24 @@ let fifth_circle = ["Fb", "Cb", "Gb", "Db", "Ab", "Eb", "Bb",      // 0 - 6
                     "F",  "C",  "G",  "D",  "A",  "E",  "B",       // 7 - 13
                     "F#", "C#", "G#", "D#", "A#", "E#", "B#"]      // 14 - 20
 
-let chordLibrary = [[1,4],[3,4],[3,6],[4,8]]
+let chordLibrary =
+[[1,4],[3,4],[3,6],[2,3,6],[1,4,5],[1,3,4],[4,8],[1],
+ [1,2],[1,2,4],[3,4,5]]
+
+let chordnames =
+["","m","dim","7","maj7","min7","+","5",
+ "sus4", "maj7(add9)","min7(add9)"]
+
+tonicIndex =
+[0, 1, 2, 1, 0, 2, 0, 0,
+ 1, 0, 1]
 
 ////////////////////////////////////////////////////////////////////////////
 
 let chord = {}
-chord.stringnotes = ["C","E","G#"]
+chord.stringnotes = ["Eb","C","G","D"]
+
+chord.bassnote = chord.stringnotes[0]
 
 chord.notes = function getnotes () {
   let array_b = []
@@ -51,15 +63,29 @@ chord.intervals = function getIntervals() {
 chord.index = function librarySearch() {
   for (i = 0; i < chordLibrary.length; i++) {
     if (arrayCompare(chord.intervals(), chordLibrary[i])) {
+      return i
       break
     }
-} return i
+} return null
 }
+
+chord.tonic = fifth_circle[chord.notes()[tonicIndex[chord.index()]]]
   
+////////////////////////////////////////////////////////////////////////////
+// Logs
 
-
-console.log(chord.notes())
 console.log(chord.intervals())
-console.log(chord.index())
+console.log(chord.notes())
+
+if (chord.index() !== null) {
+  if (chord.bassnote === chord.tonic) {
+    console.log(chord.tonic + chordnames[chord.index()])
+  }
+  else {
+    console.log(chord.tonic + chordnames[chord.index()]+"/"+chord.bassnote)
+  }
+} else {
+  console.log("Não foi possível identificar acorde")
+}
 
 
