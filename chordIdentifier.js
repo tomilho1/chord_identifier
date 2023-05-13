fd2aba0c147bdff9b7323b38020265a4a3bbe94d
Tom Francisco da Rosa <123115729+tomilho1@users.noreply.github.com>
2023-05-13 15:07:42 -0300

    adiciona updateChordData
    Primeira versão decente. O objeto chord é agora inicializado/atualizado através da função updateChordData

--- chordIdentifier.js
+++ chordIdentifier.js
@@ -25,67 +25,85 @@ let fifth_circle = ["Fb", "Cb", "Gb", "Db", "Ab", "Eb", "Bb",      // 0 - 6
 
 let chordLibrary =
 [[1,4],[3,4],[3,6],[2,3,6],[1,4,5],[1,3,4],[4,8],[1],
- [1,2],[1,2,4],[3,4,5]]
+ [1,2],[1,2,4],[3,4,5],[1,2,3],[1,4,6],[1,4,5,6],[2,3,6,8],[3,4,8],
+ [1,3,4], [1,2]]
 
 let chordnames =
 ["","m","dim","7","maj7","min7","+","5",
- "sus4", "maj7(add9)","min7(add9)"]
+ "sus4", "maj7(add9)","min7(add9)","7sus4","lyd","lyd7","7lyd","m(maj7)",
+ "6", "sus2"]
 
 tonicIndex =
 [0, 1, 2, 1, 0, 2, 0, 0,
- 1, 0, 1]
+ 1, 0, 1, 2, 0, 0, 1, 1,
+ 0, 0]
 
 ////////////////////////////////////////////////////////////////////////////
 
 let chord = {}
-chord.stringnotes = ["Eb","C","G","D"]
+chord.stringnotes = ["Eb","A","C","F"]
 
-chord.bassnote = chord.stringnotes[0]
+function updateChordData () {
 
-chord.notes = function getnotes () {
-  let array_b = []
-  for (step = 0; step < 20; step++) {
-    if (chord.stringnotes.includes(fifth_circle[step]))
-    array_b.push(step)
-    else {}
+// chord.bass
+chord.bass = chord.stringnotes[0]
+
+// chord.notes
+let array_a = []
+for (inc_a = 0; inc_a < 20; inc_a++) {
+  if (chord.stringnotes.includes(fifth_circle[inc_a]))
+    array_a.push(inc_a)
+  else {}
   }
-  return array_b
+chord.notes = array_a
+
+// chord.intervals
+let array_b = []
+  for (let inc_b = 0; inc_b < chord.notes.length - 1; inc_b++) {
+    array_b.push(chord.notes[inc_b + 1] - chord.notes[0])
+  }
+chord.intervals = array_b
+
+// chord.index
+let idx = []
+for (inc_c = 0; inc_c < chordLibrary.length; inc_c++) {
+  if (arrayCompare(chord.intervals, chordLibrary[inc_c])) {
+    idx.push(inc_c)
+  } else {}
+}
+chord.index = idx
+
+// chord.tonic
+let idx_b = []
+for (inc_d = 0; inc_d < chord.index.length; inc_d++) {
+  idx_b.push(fifth_circle[chord.notes[tonicIndex[chord.index[inc_d]]]])
+}
+chord.tonic = idx_b
+
+// chord.name
+let idx_c = []
+for (inc_e = 0; inc_e < chord.index.length; inc_e++) {
+  if (chord.bass === chord.tonic[inc_e]) {
+  idx_c.push(`${chord.tonic[inc_e]}${chordnames[chord.index[inc_e]]}`)}
+  else {
+  idx_c.push(`${chord.tonic[inc_e]}${chordnames[chord.index[inc_e]]}/${chord.bass}`)
+  }
+}
+chord.name = idx_c
+
 }
 
-chord.intervals = function getIntervals() {
-    array_b = []
-    for (let step = 0; step < chord.notes().length - 1; step++) {
-        array_b.push(chord.notes()[step + 1] - chord.notes()[0])
-    }
-    return array_b
-}
-
-chord.index = function librarySearch() {
-  for (i = 0; i < chordLibrary.length; i++) {
-    if (arrayCompare(chord.intervals(), chordLibrary[i])) {
-      return i
-      break
-    }
-} return null
-}
-
-chord.tonic = fifth_circle[chord.notes()[tonicIndex[chord.index()]]]
-  
 ////////////////////////////////////////////////////////////////////////////
 // Logs
 
-console.log(chord.intervals())
-console.log(chord.notes())
+updateChordData()
 
-if (chord.index() !== null) {
-  if (chord.bassnote === chord.tonic) {
-    console.log(chord.tonic + chordnames[chord.index()])
-  }
-  else {
-    console.log(chord.tonic + chordnames[chord.index()]+"/"+chord.bassnote)
-  }
-} else {
-  console.log("Não foi possível identificar acorde")
+for (inc = 0; inc < chord.name.length; inc++) {
+console.log(chord.name[inc])
+}
+
+if (chord.index[0] === undefined){
+  console.log("Não possível identificar este acorde")
 }
 
 
