ad0431eb59dc654f30c7d52e9262348908be6c0a
Talentos C08100N <UG_TALENTOS_C08100N@asav.brm>
2023-06-30 09:38:22 -0300

    chordForge polishment

--- chordForge.js
+++ chordForge.js
@@ -1,4 +1,5 @@
-let fifth_circle = ["Fbb", "Cbb", "Gbb", "Dbb", "Abb", "Ebb", "Bbb",
+let fifth_circle = [
+    "Fbb", "Cbb", "Gbb", "Dbb", "Abb", "Ebb", "Bbb",
     "Fb", "Cb", "Gb", "Db", "Ab", "Eb", "Bb",
     "F", "C", "G", "D", "A", "E", "B",
     "F#", "C#", "G#", "D#", "A#", "E#", "B#",
@@ -13,50 +14,57 @@ let chord = {}
 
 //
 
-let userNotes = readlineSync.question("Notes in root position: ").replaceAll(","," ").split(" ")
-
+let userNotes = readlineSync.question("Notes in root position: ").replaceAll(",", " ").split(" ")
 chord.root = userNotes[0]
 
-// The difference between chord.looseNotes and chord.orderedNotes is, of course, the order
-// in which the notes are in.
+// chord.looseNotes are the notes in the order in which they show
+// in the circle of fifths. This information which will later let
+// the program easily identify chords no matter the order the user
+// types the notes.
 
-// chord.looseNotes
+// chord.orderedNotes are the notes of the chord ordered in its root,
+// closed position.
+
 {
-let array_a = []
-for (inc_a = 0; inc_a < 35; inc_a++) {
-    if (userNotes.includes(fifth_circle[inc_a]))
-        array_a.push(inc_a)
-    else { }
-}
-chord.looseNotes = array_a
+    chord.looseNotes = []
+    fifth_circle.forEach((note, inc) => {
+        if (userNotes.includes(fifth_circle[inc]))
+            chord.looseNotes.push(inc)
+    })
 
-// chord.orderedNotes
-let array_b = []
-userNotes.forEach((note, index) => {
-    if (fifth_circle.includes(note)) {
-        if (array_b.includes(note)) {}   // Skips if it already has this note...
-        else {
-        array_b.push(note)
-    }
-    }
-    else {}
-});
-chord.orderedNotes = array_b
+    chord.orderedNotes = []
+    chord.stringNotes = []
+    userNotes.forEach((note, index) => {
+        if (fifth_circle.includes(note)) {
+            if (chord.stringNotes.includes(note)) { }   // Skips if it already has this note...
+            else {
+                chord.orderedNotes.push(fifth_circle.indexOf(note))
+                chord.stringNotes.push(note)
+            }
+        }
+        else { }
+    });
 }
 
 // This block calculates the intervals for each type of note order.
 {
-let array_a = []
-for (let inc_b = 0; inc_b < chord.looseNotes.length - 1; inc_b++) {
-    array_a.push(chord.looseNotes[inc_b + 1] - chord.looseNotes[0])
+    chord.intervals = []
+    chord.looseNotes.forEach((note, inc) => {
+        chord.intervals.push(chord.looseNotes[inc + 1] - chord.looseNotes[0])
+    })
+    chord.intervals.pop()
+    // pop() ensures chord.intervals's
+    // length is always the same as
+    // chord.looseNotes - 1.
+
+    chord.orderedIntervals = []
+    chord.orderedNotes.forEach((note, inc) => {
+        chord.orderedIntervals.push(chord.orderedNotes[inc + 1] - chord.orderedNotes[inc])
+    })
+    chord.orderedIntervals.pop()
 }
-chord.intervals = array_a
 
-//
-
-}
-
-// chord.tonicIndex corresponds to which note of chord.notes is the tonic
+// chord.tonicIndex corresponds to which note of chord.looseNotes is the tonic
 for (i = 0; i < userNotes.length; i++) {
     if (chord.root === fifth_circle[chord.looseNotes[i]]) {
         chord.tonicIndex = i
@@ -74,36 +82,31 @@ F## C## G## D## A## E## B##
 ---------------------------`
 
 // modcircl is the circle of fifths string but with
-// the notes of the chord and its root highlighted.
+// the notes of the input chord and its root highlighted.
 let modcircl = circle
-for (i = 0; i < userNotes.length; i++) {
-    let a = userNotes[i].concat("  ").slice(0, 3)
 
-    modcircl = modcircl.replace(a,"\x1b[48;5;236m"+a+"\x1b[40m")
+for (i = 0; i < userNotes.length; i++) {
+    let chordNotes = userNotes[i].concat("  ").slice(0, 3)
+
+    modcircl = modcircl.replace(chordNotes, "\x1b[48;5;236m" + chordNotes + "\x1b[40m")
 }
 
 {
-    let b = chord.root.concat("  ").slice(0, 3) // "T "
-    modcircl = modcircl.replace(b,"\x1b[48;5;241m"+b+"\x1b[40m")
+    let rootNote = chord.root.concat("  ").slice(0, 3) // "T "
+    modcircl = modcircl.replace(rootNote, "\x1b[48;5;241m" + rootNote + "\x1b[40m")
 }
 
 console.clear()
-console.log(modcircl+"\x1b[0m")
+console.log(modcircl + "\x1b[0m")
+console.log("\n", chord)
 
-console.log(`
-Notes:       ${chord.orderedNotes}
-Notes:       ${chord.looseNotes}
-Tonic:       ${chord.root}
-Tonic Index: ${chord.tonicIndex}
-Intervals:   ${chord.intervals}`)
-
-if (readlineSync.keyInYN(`
-Is this correct?`)) {
-    chord.symbol = readlineSync.question("Music notation: ")
-    chord.formalName = readlineSync.question("Formal name: ")
-} else {
-    console.log("Chord was canceled.")
-    setTimeout(() => {
-        console.clear()
-    }, "1000");
-}
+// if (readlineSync.keyInYN(`
+// Is this correct?`)) {
+//     chord.symbol = readlineSync.question("Music notation: ")
+//     chord.formalName = readlineSync.question("Formal name: ")
+// } else {
+//     console.log("Chord was canceled.")
+//     setTimeout(() => {
+//         console.clear()
+//     }, "1000");
+// }
